03dcb4742a4f153b2fbe0cb130097fee51209d40
Cenosh Xavier MJ <cenoshxavier@gmail.com>
2023-10-01 23:07:03 +0530

    basic response object use cases

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 const server=http.createServer((req,res) =>{
-  console.log('A request is made.');
+  console.log(req.url,req.method);
+
+  //Set Header content type
+  res.setHeader('Content-type','text/html');
+  res.write('<p>hello broo</p>');
+  res.write('<h1>hello broo</h1>');
+  res.end();
 });
 
 server.listen(3000,'localhost',() =>{
