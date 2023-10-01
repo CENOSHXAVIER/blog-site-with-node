41b23537fcd477b2a380a3fd8bb4414663e8114c
Cenosh Xavier MJ <cenoshxavier@gmail.com>
2023-10-01 23:31:07 +0530

    basic routing example done.

--- server.js
+++ server.js
@@ -7,8 +7,22 @@ const server=http.createServer((req,res) =>{
   //Set Header content type
   res.setHeader('Content-type','text/html');
   
+  let path = './views/';
+
+  switch(req.url){
+    case '/':
+      path+='index.html';
+      break;
+    case '/about':
+      path+='about.html';
+      break;
+    default:
+      path+='404.html';
+      break;   
+  }
+
   //Send HTML files
-  fs.readFile('./views/index.html',(err,data) => {
+  fs.readFile(path,(err,data) => {
     if(err){
       console.log(err);
       res.end();
