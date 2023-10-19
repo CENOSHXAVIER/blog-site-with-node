93b807b0a1c768447c2122a8cb0e30fe03c55ce0
Cenosh Xavier MJ <cenoshxavier@gmail.com>
2023-10-19 23:17:41 +0530

    Now using MVC architecture the whole project is organized.A "blog" folder is added inside views and create,index,details is moved to there as in future updation of the project other components can also have create,index,details files of their own.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
+const blogRoutes = require('./routes/blogRoutes');
 require('dotenv').config();
 
 // express app
@@ -21,64 +21,19 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
+//routes
 app.get("/", (req, res) => {
   res.redirect("/blogs");
 });
-
-app.get("/blogs", (req, res) => {
-  Blog.find()
-    .then((result) => {
-      res.render("index", { title: "All Blogs", blogs: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.post("/blogs", (req, res) => {
-  console.log(req.body);
-  const blog = new Blog(req.body);
-  blog
-    .save()
-    .then((result) => {
-      res.redirect("/blogs");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "Create" });
-});
-
-app.get('/blogs/:id',(req,res) =>{
-  const id = req.params.id;
-  Blog.findById(id)
-  .then((result) =>{
-    res.render('details',{blog: result , title:'Blog Details'})
-  })
-  .catch((err) =>{
-    console.log(err);
-  })
-})  
-
-app.delete('/blogs/:id',(req,res) =>{
-  const id = req.params.id;
-
-  Blog.findByIdAndDelete(id)
-  .then(result=>{
-    res.json({ redirect:'/blogs' })
-  })
-  .catch((err) =>{
-    console.log(err);
-  })
-})
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
+//blog routes
+app.use('/blogs',blogRoutes)
+
+//404 Page
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
 });
